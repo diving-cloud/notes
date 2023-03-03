19c4c60008a9f3eab27454cbf63f564f0d740c68
hezongkui <hezongkui@kuaishou.com>
2023-03-03 13:05:27 +0800

    删除一些广告

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -1,7 +1,7 @@
 import { navbar } from "vuepress-theme-hope";
 
 export const navbarConfig = navbar([
-  { text: "面试指南", icon: "java", link: "/home.md" },
+  { text: "启航", icon: "java", link: "/home.md" },
   { text: "开源项目", icon: "github", link: "/open-source-project/" },
   { text: "技术书籍", icon: "book", link: "/books/" },
   { text: "技术文章", icon: "article", link: "/high-quality-technical-articles/" },
@@ -9,7 +9,6 @@ export const navbarConfig = navbar([
     text: "网站相关",
     icon: "about",
     children: [
-      { text: "关于作者", icon: "zuozhe", link: "/about-the-author/" },
       {
         text: "更新历史",
         icon: "history",
